5bd5e54b014f38953e038ba8bd06fdbf3b264cd6
MyungJin Eun <eunmyun@uw.edu>
2017-11-26 05:49:47 -0800

    h

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -6,8 +6,7 @@ strava.activity <- read.csv(file='../data/strava_activity.csv', stringsAsFactors
 
 # Exploratory Data Analysis
 # View(head(strava.activity))
-strava.activity <- strava.activity[,c("athlete.country", "athlete.sex", "achievement_count", "average_heartrate", "average_speed", "average_watts", "distance", "elapsed_time", "elev_high", "elev_low", "kilojoules", "location_country", "max_heartrate", "max_speed", "moving_time", "total_elevation_gain", "type", "kudos_count")]
-strava.activity <- mutate(strava.activity, average_speed = distance / moving_time)
+strava.activity <- strava.activity[,c("athlete.country", "athlete.sex", "achievement_count", "average_heartrate", "average_speed", "average_watts", "distance", "elapsed_time", "kilojoules", "location_country", "max_heartrate", "max_speed", "moving_time", "total_elevation_gain", "type", "kudos_count")]
 by.type <- strava.activity %>% 
           group_by(type) %>%
           summarise(average_speed = mean(average_heartrate, na.rm=TRUE),
